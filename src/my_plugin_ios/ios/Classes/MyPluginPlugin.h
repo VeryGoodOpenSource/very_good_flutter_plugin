de9b3fdc3221e8de514a83c434bc4347f104e51c
Felix Angelov <felangelov@gmail.com>
2022-02-15 11:06:06 -0600

    feat(ios): create my_plugin_ios

---
+++ src/my_plugin_ios/ios/Classes/MyPluginPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface MyPluginPlugin : NSObject <FlutterPlugin>
+@end
---
+++ src/my_plugin_ios/ios/Classes/MyPluginPlugin.m
@@ -0,0 +1,18 @@
+#import "MyPluginPlugin.h"
+
+@implementation MyPluginPlugin
+
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar> *)registrar {
+  FlutterMethodChannel *channel =
+      [FlutterMethodChannel methodChannelWithName:@"my_plugin_ios"
+                                  binaryMessenger:registrar.messenger];
+  [channel setMethodCallHandler:^(FlutterMethodCall *call, FlutterResult result) {
+    if ([@"getPlatformName" isEqualToString:call.method]) {
+      result(@"iOS");
+    } else {
+      result(FlutterMethodNotImplemented);
+    }
+  }];
+}
+
+@end
