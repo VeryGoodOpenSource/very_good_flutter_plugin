72a4576489d5b39a924aa1c67be3664aefb7acc2
Felix Angelov <felangelov@gmail.com>
2022-02-24 13:49:49 -0600

    chore: add very_good_analysis

--- src/my_plugin_linux/test/my_plugin_linux_test.dart
+++ src/my_plugin_linux/test/my_plugin_linux_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:my_plugin_platform_interface/my_plugin_platform_interface.dart';
 import 'package:my_plugin_linux/my_plugin_linux.dart';
+import 'package:my_plugin_platform_interface/my_plugin_platform_interface.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
