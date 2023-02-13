d5aaf7571eacb00bcc7b46f097e67a4685c5b1f5
Ayad Laaouissi Jones <42848220+AyadLaouissi@users.noreply.github.com>
2023-02-13 17:17:06 +0100

    feat: use swift in iOS plugin (#85)

---
+++ src/my_plugin/my_plugin_ios/ios/Classes/MyPluginPlugin.swift
@@ -0,0 +1,14 @@
+import Flutter
+import UIKit
+
+public class MyPluginPlugin: NSObject, FlutterPlugin {
+  public static func register(with registrar: FlutterPluginRegistrar) {
+    let channel = FlutterMethodChannel(name: "my_plugin_ios", binaryMessenger: registrar.messenger())
+    let instance = MyPluginPlugin()
+    registrar.addMethodCallDelegate(instance, channel: channel)
+  }
+
+  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    result("iOS " + UIDevice.current.systemVersion)
+  }
+}
